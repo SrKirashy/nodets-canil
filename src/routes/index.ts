a80627992874bc8894427aacd789e53003c1561d
Bruno Santos Silva <jkbrunowk@hotmail.com>
2023-08-28 19:48:58 -0300

    base criada

---
+++ src/routes/index.ts
@@ -0,0 +1,17 @@
+import Routes from 'express';
+import  * as PageController from '../controllers/pageController';
+import  * as SearchController from '../controllers/searchController';
+
+const routes = Routes();
+
+routes.get('/', PageController.home);
+routes.get('/dogs', PageController.dogs);
+routes.get('/cats', PageController.cats);
+routes.get('/fishes', PageController.fishes);
+
+routes.get('/search', SearchController.search);
+
+
+
+
+export default routes;
